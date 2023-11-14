08d7f94dc059f1bd906b283eb0db4571cae961cb
Jasmine Lee <jasminelee_0409@hotmail.com>
2023-11-13 21:44:42 -0500

    Issue #41 - figured out how call youtube api

--- dev/script.js
+++ dev/script.js
@@ -12,3 +12,46 @@ $("#ingredientBtn").on("click", function () {
     },
   });
 });
+
+// var youtubeApi = "https://www.googleapis.com/youtube/v3/search?q=cookie&part=snippet&type=video&maxResults=5&key=AIzaSyCPQrlqDUzWQXG8L_DzMhfZ64M";
+// $.ajax({
+
+//   type: "GET",
+//   async: false,
+//   url: youtubeApi,
+//   success: function (result) {
+//     console.log(result);
+//   }});
+
+// // });
+
+async function searchYouTube() {
+  // const API_KEY = 'AIzaSyCPQrlqDUzWQXG8L_DzMhfZ64M-WBvCY2Q';
+  // const query = document.getElementById('query').value;
+  const url = `https://www.googleapis.com/youtube/v3/search?q=cookie&part=snippet&type=video&maxResults=5&key=AIzaSyCPQrlqDUzWQXG8L_DzMhfZ64M-WBvCY2Q`;
+
+  try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(response);
+      // displayResults(data.items);
+  } catch (error) {
+      console.error('Error fetching data:', error);
+  }
+};
+
+searchYouTube();
+
+// function displayResults(results) {
+//   const resultList = document.getElementById('results');
+//   resultList.innerHTML = '';
+
+//   results.forEach((result, index) => {
+//       const title = result.snippet.title;
+//       const videoId = result.id.videoId;
+//       const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+
+//       const listItem = document.createElement('li');
+//       listItem.innerHTML = `${index + 1}. <a href="${videoUrl}" target="_blank">${title}</a>`;
+//       resultList.appendChild(listItem);
+//   })};
